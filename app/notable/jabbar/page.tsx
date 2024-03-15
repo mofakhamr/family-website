83dc2fc9e4f733a5941e0f0eed857d59564d48ec
MofakhamR <robin@mofakham.co.uk>
2024-03-15 11:45:16 +0000

    first notable page added

---
+++ app/notable/jabbar/page.tsx
@@ -0,0 +1,17 @@
+
+export default function Home() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-5 sm:py-16">
+      <div className="sm:max-w-5xl">
+        <h1 className="">"Nazem-ol-moham"</h1>
+        <h2>Hadge-Mirza-Jabbar Taskerechi</h2>
+        <div className="text-left">
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+
+    </main>
+  );
+}
