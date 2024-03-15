8bc91304d6b1c6cad8489db0a2241e1624f82d08
MofakhamR <robin@mofakham.co.uk>
2024-03-15 17:59:08 +0000

    a better multi-use nav toggle

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -6,16 +6,20 @@ import React, { useState } from "react";
 
 // Define the Header component
 export default function Header() {
-  const [isMenuOpen, toggleNav] = React.useState(false);
-  const [isSecondOpen, toggleSecondNav] = React.useState(false);
-
-  function handleMenuClick() {
-    toggleNav(!isMenuOpen);
-    console.log(isMenuOpen);
+  type openMenus = {
+    [key: string]: boolean
   }
-  function handleSecondNav() {
-    toggleSecondNav(!isSecondOpen);
-    console.log(isSecondOpen);
+
+  const [openMenus, toggleMenuOpen] = React.useState({
+    'home': false,
+    'mobileNav': false,
+  });
+
+
+  function handleMenuItem(menuId: string) {
+    let newState = { ...openMenus };
+    newState[menuId] = !openMenus[menuId];
+    toggleMenuOpen(newState);
   }
 
   const mainMenu = [
@@ -28,10 +32,7 @@ export default function Header() {
     },
     { name: "Mausoleum", path: "/mausoleum" },
     { name: "Photographs", path: "/photographs" },
-    { name: "Contact", path: "/contact", children: [
-      { name: "Jabbar TASKRECHI", path: "/notable/jabbar" },
-      { name: "Eshagh MOFAKHAM", path: "/notable/eshagh" },
-    ] },
+    { name: "Contact", path: "/contact" },
   ]
 
   return (
@@ -52,10 +53,10 @@ export default function Header() {
         </div>
 
         <div className="sm:hidden">
-          <button onClick={handleMenuClick} type="button" className="mr-5 block text-black focus:text-black focus:outline-none hover:text-black">
+          <button onClick={() => handleMenuItem('mobileNav')} type="button" className="mr-5 block text-black focus:text-black focus:outline-none hover:text-black">
             <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path className={isMenuOpen ? 'block' : 'hidden'} fillRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
-              <path className={!isMenuOpen ? 'block' : 'hidden'} fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+              <path className={openMenus['mobileNav'] ? 'block' : 'hidden'} fillRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
+              <path className={!openMenus['mobileNav'] ? 'block' : 'hidden'} fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
             </svg>
           </button>
         </div>
@@ -65,27 +66,27 @@ export default function Header() {
       {/*<!-- Main Nav -->*/}
       <div className='text-white'>
         {/*<!-- Primary Nav -->*/}
-        <nav className={`px-2 pt-2 pb-4 sm:mr-20 sm:flex sm:p-0 ${isMenuOpen ? 'block' : 'hidden'}`}>
+        <nav className={`px-2 pt-2 pb-4 sm:mr-20 sm:flex sm:p-0 ${openMenus['mobileNav'] ? 'block' : 'hidden'}`}>
           <div className="sm:flex">
             <div>
               <a key="nmx" href="/" className='block px-2 py-1 text-white sm:text-slate-700 hover:text-black font-semibold rounded hover:bg-gray-200'>Home</a>
             </div>
 
             {mainMenu.map((item, i) => {
-              let hasChildren:JSX.Element | undefined;
+              let hasChildren: JSX.Element | undefined;
               const children = (item.children) && item.children.map((child, si) => {
                 hasChildren = <i className="arrow adown"></i>;
-                return <div key={i + si} className="block ml-4 mt-1"><a href={child.path} className=''>{child.name}</a></div>;
+                return <div key={i + si} className="block m-1 mt-1 p-1 sm:p-2 hover:bg-gray-100 hover:text-black rounded"><a href={child.path} className=''>{child.name}</a></div>;
               })
-
+              const menuId = i + 1;
               return <div key={i}>
                 <a href={item.path}
-                onClick={hasChildren && handleSecondNav}
-                className='block mt-1 px-2 py-1 text-white sm:text-slate-700 hover:text-black font-semibold rounded hover:bg-gray-200 sm:mt-0 sm:ml-2'>
+                  onClick={() => handleMenuItem(menuId.toString())}
+                  className='block mt-1 px-2 py-1 text-white sm:text-slate-700 hover:text-black font-semibold rounded hover:bg-gray-200 sm:mt-0 sm:ml-2'>
                   {item.name}
                   {hasChildren && hasChildren}
                 </a>
-                <div className={isSecondOpen ? 'block' : 'hidden'}>{children}</div>
+                <div className={openMenus[menuId.toString()] ? 'sm:fixed block bg-gray-500 ml-2 p-2' : 'hidden'}>{children}</div>
               </div>;
             })}
           </div>
